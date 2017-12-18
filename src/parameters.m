d270b6f8a278f2bb4c22367111f0a4dcc5c7bc5d
Thomas eppenberger <thomasep@student.ethz.ch>
2017-12-19 00:24:04 +0100

    params...

--- src/parameters.m
+++ src/parameters.m
@@ -23,19 +23,21 @@ switch(ds)
         bootstrap.desc_method = 'HOG';
 
         bootstrap.use_KLT = true; 
-        bootstrap.ransac_iter = 100; %nbr of estimateFundamentalMatrix runs
+        bootstrap.ransac_iter = 1000; %nbr of estimateFundamentalMatrix runs
 
             % harris
             harris.min_quality = 0.001; % 0.001 init
             harris.num_points = 10000; % 10000 init
-            harris.min_quality_process = 0.001; 
-            harris.num_points_process = 10000; 
+            harris.min_quality_process = 0; 
+            harris.num_points_process = 1000; 
 
             % fast
             fast.min_quality = 0.01;
             fast.num_points = 20000; 
 
-        select_uniform.delta = 4; %radius for non-maximum supression
+        select_uniform.delta = 8; %radius for non-maximum supression
+        select_uniform.nbr_pts = 600;
+        select_uniform.viaMatrix_method=true; %true for matrix filling approach, false for keypoints_loc method
             
         ransac.numTrials = 10000; %ransac inside of estimateFundamentalMatrix
         ransac.distanceThreshold = 0.01; 
@@ -48,28 +50,27 @@ switch(ds)
         % CONTINUOUS OPERATION
 
         % KLT point Tracker
-        klt.NumPyramidLevels = 3; % TODO: nbr was mentioned in lecture
-        klt.MaxBidirectionalError = 5; %5 % if inf, is not calculated
-        klt.BlockSize = [31 31];
-        klt.MaxIterations = 1000;
+        klt.NumPyramidLevels = 5; % TODO: nbr was mentioned in lecture
+        klt.MaxBidirectionalError = 2; %5 % if inf, is not calculated
+        klt.BlockSize = [37 37]; %init 31 31
+        klt.MaxIterations = 1000; %init 50
         
         % isClose fct
-        is_close.delta = 1; %0.5; %in pixel, treated as same keypoint if within delta
+        is_close.delta = 6; %0.5; %in pixel, treated as same keypoint if within delta
 
         % p3p 3D-2D algo
         p3p.p3p_and_ransac_iter = 3;
         
             % ransac inside of runP3PandRANSAC
-            p3p_ransac.num_iteration = 1000;
-            p3p_ransac.pixel_tolerance = 5; % 2 init, better
-            p3p_ransac.min_inlier = 10;
+            p3p_ransac.num_iteration = 10000;
+            p3p_ransac.pixel_tolerance = 4; % 2 init, better
+            p3p_ransac.min_inlier = 6;
             
         p3p.max_delta_loc = 1;
-        
-
+       
         % triangulation
-        triang.alpha_threshold = deg2rad(5); % 20 init
-        triang.rep_e_threshold = 0.2; %init 3 % max allowed reprojection error in triangulation
+        triang.alpha_threshold = deg2rad(10); % 20 init
+        triang.rep_e_threshold = 200; %init 3 % max allowed reprojection error in triangulation
         
 
         % detect new candidate kp
@@ -77,8 +78,7 @@ switch(ds)
         
             % harris
             harris.selectUniform = true;
-            processFrame.harris.num_points_process = 100;
+            processFrame.harris.num_points_process = 1000;
             processFrame.harris.min_quality_process = 0.001;
-
-            
+    
 end
